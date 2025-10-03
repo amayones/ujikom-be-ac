3e0e52b347783aede7d40eae48007031213f03ba
amayones <mayonedrive1@gmail.com>
2025-10-03 15:33:34 +0700

    Add APP_KEY and fix permissions

--- generate-app-key.php
+++
@@ -1,9 +0,0 @@
-<?php
-// Generate Laravel APP_KEY
-$key = 'base64:' . base64_encode(random_bytes(32));
-echo "Add this to your task-definition.json:\n";
-echo "{\n";
-echo '  "name": "APP_KEY",'."\n";
-echo '  "value": "'.$key.'"'."\n";
-echo "}\n";
-?>
